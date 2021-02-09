54002b8a5ca1ff0544e03b3c7a17432f8c57d57a
NastyaButenko <73394674+NastyaButenko@users.noreply.github.com>
2021-02-09 18:21:12 +0300

    lab21_9.02

---
+++ javafx_primer/src/sample/Main.java
@@ -0,0 +1,25 @@
+package sample;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    private Stage stage;
+    private AnchorPane anchorPane;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        stage = primaryStage;
+        anchorPane = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        stage.setScene(new Scene(this.anchorPane));
+        stage.setTitle("Работа с визуальными табличными данными");
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
