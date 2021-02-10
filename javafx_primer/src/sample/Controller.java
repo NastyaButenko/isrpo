036df787240ce33e7a173c4d0483b6a0fa1bf6cd
NastyaButenko <73394674+NastyaButenko@users.noreply.github.com>
2021-02-10 11:00:57 +0300

    lab21_9.02

--- javafx_primer/src/sample/Controller.java
+++ javafx_primer/src/sample/Controller.java
@@ -5,6 +5,7 @@ import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.TextFieldTableCell;
